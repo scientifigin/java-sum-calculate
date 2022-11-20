505cafde0c641940c04ed3a6d746efd03f8de6b6
GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>
2022-11-20 19:13:31 +0530

    Update calculatejavasum.java
    Signed-off-by: GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>

--- calculatejavasum.java
+++ calculatejavasum.java
@@ -1,4 +1,5 @@
-package com.company;
+// For Addition 
+package scientifigin;
 import java.util.Scanner;
 
 public class Thirdjava {
@@ -10,6 +11,60 @@ public class Thirdjava {
         System.out.println("Enter number 2");
         int b = sc.nextInt();
         int sum = a+b;
+        System.out.println("The sum of these number is");
+        System.out.println(sum);
+    }
+}
+
+// For Substruction 
+package scientifigin;
+import java.util.Scanner;
+
+public class Thirdjava {
+    public static void main(String[] arg) {
+        System.out.println("Taking Input From The User");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        int sum = a-b;
+        System.out.println("The sum of these number is");
+        System.out.println(sum);
+    }
+}
+
+// For Multiplication 
+package scientifigin;
+import java.util.Scanner;
+
+public class Thirdjava {
+    public static void main(String[] arg) {
+        System.out.println("Taking Input From The User");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        int sum = a*b;
+        System.out.println("The sum of these number is");
+        System.out.println(sum);
+    }
+}
+
+// For Division 
+package scientifigin;
+import java.util.Scanner;
+
+public class Thirdjava {
+    public static void main(String[] arg) {
+        System.out.println("Taking Input From The User");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        int sum = a/b;
         System.out.println("The sum of these number is");
         System.out.println(sum);
     }
